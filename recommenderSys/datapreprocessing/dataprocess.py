a63124c9c946d32fead357b4b63228ccb5714b6e
leo <liuzhi7936@sina.cn>
2016-04-28 10:17:08 +0800

    add formated data and some methods

--- recommenderSys/datapreprocessing/dataprocess.py
+++ recommenderSys/datapreprocessing/dataprocess.py
@@ -25,6 +25,23 @@ def extraUser_Merchant(data):
         user_merchant[user] = data[data[:,0] == user][:,1:length]
     return user_merchant
 
+# this method used to extract merchants in a specific location
+def extraLocation_merchant(data):
+    locate_merchant = dict();
+    for line in data:
+        locate = line[-1]
+        if ':' in locate:
+            locate = locate.split(':')
+            for l in locate:
+                if not l in locate_merchant:
+                    locate_merchant[l] = set()
+                locate_merchant[l].add(line[0])
+        else: 
+            if not locate in locate_merchant:
+                locate_merchant[locate] = set()
+            locate_merchant[locate].add(line[0])
+    return locate_merchant
+
 # this method used to define the user rate from implicit feedback, the format is user:{merchant:number of consumption}
 # ignore the location information
 def extraUserRecord(user_merchant):
@@ -46,12 +63,27 @@ def computeUserRates(user_record):
             if subval >= 3: 
                 user_rate[key][subkey] = 3 + (subval - 3)//3
                 if user_rate[key][subkey] > 5: user_rate[key][subkey] = 5
-            
             else: 
                 user_rate[key][subkey] = subval;
         print(key, user_rate[key])
     return user_rate;
 
+# this method used to compute the number of consumption for each merchant in a specific location
+def calculateRateInLocation(location_merchants, originalData):
+    location_mers_rate = dict();
+#     for k,v in location_merchants.items():
+#         location_mers_rate[k] = dict()
+#         for merchant in v:
+#             location_mers_rate[k][merchant] = 0
+    for record in originalData:
+        if not record[2] in location_mers_rate:
+            location_mers_rate[record[2]] = dict()
+        if not record[1] in location_mers_rate[record[2]]:
+            location_mers_rate[record[2]][record[1]] = 0
+        location_mers_rate[record[2]][record[1]] += 1
+    return location_mers_rate
+
+# this method used to store data which its' format is key:{subkey:value}
 def storeData(data, toFile2):
     print('Begin to store!')
     with open(toFile2, 'wt') as handle:
@@ -60,10 +92,24 @@ def storeData(data, toFile2):
             for subkey,subval in val.items():
                 line = line + str(subkey) + "-" + str(subval) + ","
             line = line[:-1]
+            print(line)
             handle.write(line + '\n')
             
+def store_dictData(data, toFile):
+    with open(toFile, 'wt') as dicthandle:
+        for k,v in data.items():
+            line = k + ':'
+            for val in v:
+                line = line + val + '-'
+            line = line[:-1]
+            dicthandle.write(line + "\n");
+            
 if __name__ == '__main__':
-#     data = formatData('data/ijcai2016_koubei_train')
+    data = formatData('data/ijcai2016_koubei_train')
+    location_merchant = pickle.load(open('data/location_merchant', 'rb'))
+    location_merchant_rate = calculateRateInLocation(location_merchant, data)
+    storeData(location_merchant_rate, 'data/location_merchant_rate_text')
+    pickle.dump(location_merchant_rate, open('data/location_merchant_rate', 'wb'))
 #     with open('data/merchantIds','rb') as merchantshandle:
 #         merchants = pickle.load(merchantshandle)
 #     merchants = [int(merchant) for merchant in merchants]
@@ -74,12 +120,12 @@ if __name__ == '__main__':
 #     print('Begin to store data!')
 #     with open('data/rate', 'wb') as ratehandle:
 #         pickle.dump(user_rate, ratehandle)
-    with open('data/user_records', 'rb') as user_merhandle:
-        user_records = pickle.load(user_merhandle)
-    user_rate = computeUserRates(user_records)
+#     with open('data/user_records', 'rb') as user_merhandle:
+#         user_records = pickle.load(user_merhandle)
+#     user_rate = computeUserRates(user_records)
 #     with open('data/user_rate', 'wb') as ratehandle:
 #         pickle.dump(user_rate, ratehandle)
-    storeData(user_rate, 'data/user_rate_text')
+#     storeData(user_rate, 'data/user_rate_text')
 #     data = np.array(data);
 #     user_merchant = extraUser_Merchant(data)
 #     storeData(user_merchant, 'data/user_merchant_diy', 'data/user_merchant_py')
--- recommenderSys/datapreprocessing/recsys.py
+++ recommenderSys/datapreprocessing/recsys.py
@@ -5,10 +5,59 @@ Created on 2016年4月26日
 '''
 import pickle
 import numpy as np
+import copy
 
+# The format of u and v are user:{merchant-rate, ..., merchant-rate}（使用皮尔森相似度）
 def computeSim(u, v):
-    return -1
+    merchant_u = set(u.keys())
+    merchant_v = set(v.keys())
+    print(merchant_u ,' ', merchant_v)
+    intersection = merchant_u & merchant_v
+    if len(intersection) == 0: return 0;
+    umean = np.mean(list(u.values())); vmean = np.mean(list(v.values()))
+    numeru = 0; denou = 0; denov = 0;
+    for merchant in intersection:
+        numeru = numeru + (u[merchant] - umean) * (v[merchant] - vmean)
+        denou = denou + (u[merchant] - umean) ** 2
+        denov = denov + (v[merchant] - vmean) ** 2
+    pc_u_v = numeru / np.sqrt(denou * denov)
+    return pc_u_v
 
+def computeNearestNei():
+
+def calculateRec(neighbors=5,*, testdata, user_rate, submitFile):
+    location_merchant_rate = pickle('data/location_merchant_rate', 'rb')
+    with open(submitFile, 'wt') as submithandle:
+        for test in testdata:
+            if not test[0] in user_rate:
+                merchant_local = copy.deepcopy(location_merchant_rate[test[0]])
+                rec_test = test[0] + ',' + test[1] + ','
+                if len(merchant_local) <= 10:
+                    for merchant in merchant_local.keys():
+                        rec_test = rec_test + merchant + ':'
+                    rec_test = rec_test[:-1]
+                else:
+                    for i in range(1,1,11):
+                        merchant = max_ith(merchant_local,i)
+                        rec_test = rec_test + merchant + ':'
+                print(rec_test)
+                submithandle.write(rec_test + '\n')
+            else:
+                testUser = 
+    
+# this method used to find the ith largest element in a dict
+def max_ith(data, i):
+    i_max_merchant = ''; i_max_rate = 0
+    for k,v in data.items():
+        if i_max_rate < v: i_max_rate = v; i_max_merchant = k
+    data.pop(i_max_merchant)
+    return i_max_merchant,i_max_rate;
 
 if __name__ == '__main__':
+    location_merchant_rate = pickle.load(open('data/location_merchant_rate', 'rb'))
+    print(location_merchant_rate['352'])
+    merchant_rate = copy.deepcopy(location_merchant_rate['352'])
+    merchant,rate = max_ith(location_merchant_rate['352'],1)
+    print('the number of historical consumption of merchant {0} is {1}'.format(merchant, rate))
+    
     
